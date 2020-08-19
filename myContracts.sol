4ae3f96f651c41247439426d440700d15c2c7e4d
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-08-19 22:20:59 +0530

    commit 3

--- myContracts.sol
+++ myContracts.sol
@@ -1,13 +1,52 @@
 pragma solidity 0.5.1;
 
 contract myContract {
+    
+    //Example 1 : Basic syntax.
+    
     string public value;
     
-    constructor() public {
-        value = "myValue";
-    } 
-    
-    function set(string memory _value) public {
-        value = _value;
+    function set(string memory value1) public {
+        value = value1;
     }
+    
+    //Example 2 : modifiers and access specifiers (Unix Epoch Clock).
+
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    uint256 openingTime = 1597854699;
+
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= openingTime);
+        _;
+    }
+
+    uint256 public peopleCount=0 ;
+    mapping(uint => Person) public people;
+    
+    struct Person {
+        uint _id;
+        string _firstname;
+        string _lastname;
+    }
+    
+    function incrementCount() internal {
+        peopleCount += 1;
+    }
+
+    function addPerson(string memory _firstname,string memory _lastname) public onlyWhileOpen onlyOwner {
+        incrementCount();
+        people[peopleCount] = Person(peopleCount,_firstname,_lastname);
+    }
+
+
 }
