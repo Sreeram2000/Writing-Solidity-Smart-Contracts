ab8dace865b3d4b75369efe617869e70908a61da
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-09-27 22:12:33 +0530

    Currency.sol

--- mycontract4.sol
+++ mycontract4.sol
@@ -1,14 +1,30 @@
-pragma solidity 0.6.1;
-contract metaCoin {	
-    uint256 public value=100;
-	mapping (address => uint) balances;
-	constructor() public{
-		balances[msg.sender] = 10;
-	}
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		return true;
-	}
+pragma solidity ^0.4.0;
+
+contract Subcurrency {
+    
+ address public minter;
+ mapping (address => uint) public coinBalances;
+ 
+ event Sent(address from, address to, uint sum);
+ 
+ function Coin() {
+   minter = msg.sender;
+ }
+ 
+ function mint(address receiver, uint sum) {
+   if (msg.sender != minter) return;
+   coinBalances[receiver] += sum;
+ }
+ 
+ function send(address receiver, uint sum) {
+   if (coinBalances[msg.sender] < sum) return;
+   coinBalances[msg.sender] -= sum;
+   coinBalances[receiver] += sum;
+   Sent(msg.sender, receiver, sum);
+ }
+ 
+ function getcoinBalances(address _account) returns (uint) {
+   return coinBalances[_account];
+}
+
 }
